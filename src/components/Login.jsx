fe7ab9947873328b979b8dcf1d3c1c95afff6c99
devdeepak06 <k.deepak2217@gmail.com>
2025-02-21 13:01:51 +0530

    setup the router for login and profile component

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold">
+        Hello Login page!
+      </h1>
+    </>
+  )
+}
+
+export default Login;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold">
+        Hello Profile page!
+      </h1>
+    </>
+  )
+}
+
+export default Profile;
