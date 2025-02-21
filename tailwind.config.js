b3636948d61aa60bc4a84dede824f088881d49b6
devdeepak06 <k.deepak2217@gmail.com>
2025-02-21 12:28:35 +0530

    setup the daisyui tailwindcss and install the react route dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    daisyui,
+    daisyui
   ],
 }
+
