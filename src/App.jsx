dba6d2c342065f878d717204f017e506636ce80f
devdeepak06 <k.deepak2217@gmail.com>
2025-02-24 01:03:36 +0530

    basic login api setup done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Signup from "./components/Signup";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Body />}>
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/signup" element={<Signup />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+
 const Login = () => {
+  const [email, setEmail] = useState("deepak.kumar@google.com");
+  const [password, setPassword] = useState("Deepak@123");
+
+  const handleLogIn = async () => {
+   try {
+     const result = await axios.post(`${apiUrl}/api/auth/login`, {
+       email, password
+     }, { withCredentials: true });
+     console.log(result);
+   } catch (error) {
+     console.error(error)
+    
+   }
+  }
   return (
     <>
-      <h1 className="text-3xl text-center font-bold">
-        Hello Login page!
-      </h1>
+      <div className="flex justify-center items-center py-40">
+        <div className="card bg-base-300 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title flex justify-center">Login</h2>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input type="text" placeholder="example@gmail.com" className="input input-bordered w-full max-w-xs"
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input type="text" placeholder="********" className="input input-bordered w-full max-w-xs"
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+              />
+            </label>
+            <div className="card-actions justify-center">
+              <button onClick={handleLogIn} className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-md">Login</button>
+              <button className="btn btn-neutral btn-xs sm:btn-sm md:btn-md lg:btn-md">
+                <a href="/signup">
+                  Signup
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
