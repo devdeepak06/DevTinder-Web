e04b8e6c12235cc958b82b170353e79c436685c8
devdeepak06 <k.deepak2217@gmail.com>
2025-03-06 23:42:23 +0530

    updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
-<<<<<<< HEAD
-import Footer from './Footer';
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "../utils/useSlice";
-import { useEffect } from "react";
-const apiUrl = import.meta.env.VITE_API_URL;
-=======
 import Footer from "./Footer";
 import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -15,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useEffect } from "react";
 
->>>>>>> 1ece36bbc5d842c4677162238d89c34cf5725c33
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -25,28 +16,13 @@ const Body = () => {
     if (userData) return;
     try {
       const res = await axios.get(apiUrl + "/profile/view", {
-<<<<<<< HEAD
-        withCredentials: true
-      });
-      dispatch(addUser(res.data))
-=======
         withCredentials: true,
       });
       dispatch(addUser(res.data));
->>>>>>> 1ece36bbc5d842c4677162238d89c34cf5725c33
     } catch (err) {
       if (err.status === 401) {
         navigate("/login");
       }
-<<<<<<< HEAD
-      console.error(err)
-    }
-  }
-
-  useEffect(() => {
-    fetchUser();
-  }, [])
-=======
       console.error(err);
     }
   };
@@ -54,7 +30,6 @@ const Body = () => {
   useEffect(() => {
     fetchUser();
   }, []);
->>>>>>> 1ece36bbc5d842c4677162238d89c34cf5725c33
 
   return (
     <div>
